ea894b6bce8fc5208cf07121e242f4dbf6a43df3
meganung <ung.meg@gmail.com>
2019-02-09 05:39:49 -0500

    audio pop up current tab only once

--- audioalert/background.js
+++ audioalert/background.js
@@ -4,41 +4,13 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
+var alerted = new Array();
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    var checkTabs = new Array();
-    chrome.tabs.query({audible: true}, function (tabs) {
-        for (var i = 0; i < tabs.length; i++) {
-            checkTabs[i] = tabs[i];
-        }
+  
+  if (tab.audible && !alerted.includes(tabId)){
+    window.alert("This tab is playing audio!");
+    alerted.push(tabId);
+  }
 
-        if (checkTabs.length == 0)
-          window.console.log("NONE");
-        else
-          chrome.extension.getBackgroundPage().alert('Music is playing!');
-
-        for (var i = 0; i < checkTabs.length; i++) {
-            if (checkTabs[i] != null) {
-              window.console.log("ON");
-              window.console.log(checkTabs[i].url); 
-            } else {
-              window.console.log("OFF");
-              window.console.log("??" + i);
-            }
-        }
-        console.log("qu");
-    });
 });
